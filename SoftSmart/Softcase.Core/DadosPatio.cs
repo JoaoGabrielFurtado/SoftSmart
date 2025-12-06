06ff82a88a7efcd4519f0620704fce6cfbce216d
João Gabriel <sospox8@gmail.com>
2025-12-06 13:04:12 -0300

    Iniciando o projeto

---
+++ SoftSmart/Softcase.Core/DadosPatio.cs
@@ -0,0 +1,18 @@
+﻿namespace Softcase.Core
+{
+    public class DadosPatio
+    {
+        //OBS: declarei todas as props como float pois o ML entende melhor. 
+
+        // Entradas 
+        public float Hora { get; set; }           
+        public float DiaDaSemana { get; set; }    
+        public float Temperatura { get; set; }    
+        public float Chuva { get; set; }          
+        public float EventoNaRegiao { get; set; } 
+        public float EhFeriado { get; set; }      
+
+        // Saída 
+        public float Ocupacao { get; set; }       
+    }
+}
---
+++ SoftSmart/Softcase.ML/Program.cs
@@ -0,0 +1,10 @@
+﻿
+// Variaveis
+using Softcase.Core;
+
+DateTime dataInicial = DateTime.Now.AddYears(-1);
+DateTime dataFinal = DateTime.Now;
+int vagasTotais = 250;
+
+List<DadosPatio> Lista_Patio = new();
+
